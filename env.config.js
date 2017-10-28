77ea62e17e57bf39593b8a751de57350c9fdeb76
Kasper Mikiewicz <IderedPL@gmail.com>
2017-10-28 21:56:16 +0200

    chore: change fb

--- env.config.js
+++ env.config.js
@@ -1,8 +1,8 @@
 const ENV = {
-  master: ''
+  master: 'PRODUCTION'
 }[process.env.CIRCLE_BRANCH || 'devel']
 
-const envVars = ['SYNCANO_PROJECT_INSTANCE']
+const envVars = ['SYNCANO_PROJECT_INSTANCE', 'FACEBOOK_APP_ID']
 
 const variables = envVars.reduce((env, key) => {
   const name = ENV ? `${ENV}_${key}` : key
--- src/views/landing/index.js
+++ src/views/landing/index.js
@@ -6,6 +6,7 @@ import {
   Nav,
   Head,
   Page,
+  Grid,
   Link,
   MatchAsGuest,
   ProgressBar
@@ -24,46 +25,45 @@ const Landing = ({logout}) => (
       <div className="Main">
         <h2 className="Main__title u-pt+++">Democracy 3.0</h2>
 
-        <div className="CurrentQuestion u-mt+++">
-          <div>
-            <div className="CurrentQuestion__title">
-              Current question
-            </div>
-
-            <h1 className="CurrentQuestion__content">
-              Should we do that or that?
-            </h1>
-          </div>
-
-          <div className="CurrentQuestion__answers">
-            <div className="CurrentQuestion__title">
-              Results
-            </div>
-            <div className="CurrentQuestion__answer">
-              <div className="CurrentQuestion__answer-head">
-                <span className="CurrentQuestion__key">Yes</span>
-                <span className="CurrentQuestion__value">{Math.round(100 * 20 / 124)}% (20)</span>
-              </div>
-              <ProgressBar total={124} value={20} />
-            </div>
-            <div className="CurrentQuestion__answer">
-              <div className="CurrentQuestion__answer-head">
-                <span className="CurrentQuestion__key">No</span>
-                <span className="CurrentQuestion__value">{Math.round(100 * 104 / 124)}% (104)</span>
-              </div>
-              <ProgressBar total={124} value={104} />
-            </div>
-          </div>
-        </div>
-
-        <div className="u-mt">
+        <div className="u-mt+++">
           <QuestionForm />
         </div>
 
-        <h2 className="u-mt">Hot questions</h2>
-        <div className="u-mt---">
-          <QuestionList />
-        </div>
+        <Grid spacing="md">
+          <div className="u-1/1">
+            <h2 className="u-mt">Pending questions</h2>
+            <div className="u-mt---">
+              <QuestionList />
+            </div>
+          </div>
+          <div className="u-1/1">
+            <h2 className="u-mt">Asked questions</h2>
+            <div className="u-mt---">
+              <div className="CurrentQuestion">
+                <h1 className="CurrentQuestion__content">
+                  Should we do that or that?
+                </h1>
+
+                <div className="CurrentQuestion__answers u-mt---">
+                  <div className="CurrentQuestion__answer">
+                    <div className="CurrentQuestion__answer-head">
+                      <span className="CurrentQuestion__key">Yes</span>
+                      <span className="CurrentQuestion__value">{Math.round(100 * 20 / 124)}% (20)</span>
+                    </div>
+                    <ProgressBar total={124} value={20} />
+                  </div>
+                  <div className="CurrentQuestion__answer">
+                    <div className="CurrentQuestion__answer-head">
+                      <span className="CurrentQuestion__key">No</span>
+                      <span className="CurrentQuestion__value">{Math.round(100 * 104 / 124)}% (104)</span>
+                    </div>
+                    <ProgressBar total={124} value={104} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Grid>
       </div>
     </Wrapper>
 
@@ -79,7 +79,6 @@ const Landing = ({logout}) => (
       }
 
       .CurrentQuestion {
-        display: flex;
         justify-content: space-between;
         border: 1px solid #e5e5e5;
         padding: 32px;
